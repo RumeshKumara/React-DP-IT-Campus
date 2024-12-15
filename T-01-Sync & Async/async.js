5cf6af2726a5a835c96cfd328193e0cbae2a4994
rumesh <rumeshk066@gmail.com>
2024-12-15 16:36:41 +0530

    Add-1

---
+++ T-01-Sync & Async/async.js
@@ -0,0 +1,16 @@
+function func(){
+    console.log(1);
+    console.log(1);
+    console.log(1);
+    console.log(1);
+    console.log(1);
+
+}
+
+setTimeout(func, 3000);
+// func();
+console.log(2);
+console.log(2);
+console.log(2);
+console.log(2);
+console.log(2);
